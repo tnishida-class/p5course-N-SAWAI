0872a467c1a3cfb8b223b6639ce37d07b2ad6e49
N-SAWAI <s2116to@gmail.com>
2021-10-24 23:59:53 +0900

    change section 2-q1
    半径を工夫することで少しは見やすい円になったと思います。

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,17 @@
 // 小手調べ
+
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
-  }
+
+for(let i = 0; i < 10; i++){
+noFill();
+if(i < 4){
+stroke(0,0,255)
+ellipse(50,50,i*5+10)
+}
+else{
+  stroke(255,0,0)
+  ellipse(50,50,i*5+10)
+}
+  // BLANK[1]
+}
 }
