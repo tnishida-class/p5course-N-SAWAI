53d275a5ac8c917c1a7d2436fd7f287a166e5618
N-SAWAI <s2116to@gmail.com>
2021-11-08 01:06:09 +0900

    change section4-2
    ボールのサイズは20～40にしています。
    何もせずにわらわら出現するボールは心臓の課題で使用したスキルを応用させました。

--- section4-2/sketch.js
+++ section4-2/sketch.js
@@ -3,9 +3,13 @@
 // 練習問題：何も操作しなくてもボールが湧いてくる機能を追加しよう
 
 let balls;
+let count;
+let cycle;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
+  count=0;
+  cycle=200;
   balls = [];
 }
 
@@ -17,13 +21,19 @@ function draw(){
     b.x += b.vx;
     b.y += b.vy;
   }
+
+  count = (count + 1) % cycle;
+  if(count<100){
+    const b = { x: width/3, y: width/3, size: 10, vx: random(-20,20), vy: random(-20,20) };
+    balls.push(b);
+  }
 }
 
 function mouseDragged(){
   const dx = mouseX - pmouseX;
   const dy = mouseY - pmouseY;
   if(mag(dx, dy) > 5){
-    const b = { x: mouseX, y: mouseY, size: 20, vx: dx, vy: dy };
+    const b = { x: mouseX, y: mouseY, size: random(20,40), vx: dx, vy: dy };
     balls.push(b);
   }
 }
